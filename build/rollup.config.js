9f2db962f9b5525abcd002748168dc1d12bedb79
Tamas Besenyei <tamas@besenyei.net>
2019-05-08 15:50:49 +0200

    finalize docs and package.json

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,16 +3,16 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * Copyright (c) 2019 (original work) Open Assessment Technologies SA ;
  */
 
@@ -21,9 +21,9 @@ import glob from 'glob';
 import alias from 'rollup-plugin-alias';
 import json from 'rollup-plugin-json';
 import copy from 'rollup-plugin-copy';
-import {uglify} from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 
-const {srcDir, outputDir} = require('./path');
+const { srcDir, outputDir } = require('./path');
 
 const inputs = glob.sync(path.join(srcDir, '!(lib)', '**', '*.js'));
 const localExternals = inputs.map(input => path.relative(srcDir, input).replace(/\.js$/, ''));
@@ -56,25 +56,25 @@ export default inputs.map(input => {
             'lib/store/idbstore',
             'lib/decimal/decimal',
             'lib/expr-eval/expr-eval',
-            'ui/feedback',
-        ].concat(localExternals).concat(libExternals),
+            'ui/feedback'
+        ]
+            .concat(localExternals)
+            .concat(libExternals),
         plugins: [
             alias({
                 resolve: ['.js', '.json'],
                 core: path.resolve(srcDir, 'core'),
                 util: path.resolve(srcDir, 'util'),
                 lib: path.resolve(srcDir, 'lib')
-              }),
+            }),
             json({
                 preferConst: false
             }),
             copy({
-                targets: [
-                  path.resolve(srcDir, 'lib')
-                ],
-                outputFolder: outputDir,
+                targets: [path.resolve(srcDir, 'lib')],
+                outputFolder: outputDir
             }),
-            // uglify()
+            uglify()
         ]
     };
 });
