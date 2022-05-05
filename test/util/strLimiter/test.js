2bab06f9e7ad021d2218dc1a8be1a0f78ce1dd1e
jsconan <jsconan@gmail.com>
2022-05-05 16:43:48 +0200

    test(strLimiter): prepare test cases for the limit by chars

--- test/util/strLimiter/test.js
+++ test/util/strLimiter/test.js
@@ -15,100 +15,110 @@
  *
  * Copyright (c) 2018-2022 Open Assessment Technologies SA
  */
-define(['util/strLimiter'], function(strLimiter) {
+define(['util/strLimiter'], function (strLimiter) {
     'use strict';
 
     QUnit.module('strLimiter');
 
-    QUnit.cases.init([{
-        title: 'plain text',
-        input: 'Lorem ipsum dolor sit amet',
-        unlimited: 'Lorem ipsum dolor sit amet',
-        limited: 'Lorem ipsum'
-    }, {
-        title: 'plain text with long spaces',
-        input: '  Lorem    ipsum   dolor  sit   amet  ',
-        unlimited: '  Lorem    ipsum   dolor  sit   amet',
-        limited: '  Lorem    ipsum'
-    }, {
-        title: 'simple HTML',
-        input: '<p>Lorem ipsum dolor sit amet</p>',
-        unlimited: '<p>Lorem ipsum dolor sit amet</p>',
-        limited: '<p>Lorem ipsum</p>'
-    }, {
-        title: 'glued HTML',
-        input: '<p> Lorem </p><p> ipsum <br></p><p> dolor </p><p> sit </p><p> amet </p>',
-        unlimited: '<p> Lorem </p><p> ipsum <br></p><p> dolor </p><p> sit </p><p> amet </p>',
-        limited: '<p> Lorem </p><p> ipsum <br></p>'
-    }]).test('limitByWordCount ', (data, assert) => {
-        assert.equal(strLimiter.limitByWordCount(data.input, 5), data.unlimited, 'Limited by 5 words');
-        assert.equal(strLimiter.limitByWordCount(data.input, 10), data.unlimited, 'Limited by 10 words');
-        assert.equal(strLimiter.limitByWordCount(data.input, 2), data.limited, 'Limited by 2 words');
-        assert.equal(strLimiter.limitByWordCount(data.input, 0), '', 'Limited by 0 words');
-        assert.equal(strLimiter.limitByWordCount(data.input, -2), '', 'Limited by negative value');
-    });
+    QUnit.cases
+        .init([
+            {
+                title: 'plain text',
+                input: 'Lorem ipsum dolor sit amet',
+                unlimited: 'Lorem ipsum dolor sit amet',
+                limited: 'Lorem ipsum'
+            },
+            {
+                title: 'plain text with long spaces',
+                input: '  Lorem    ipsum   dolor  sit   amet  ',
+                unlimited: '  Lorem    ipsum   dolor  sit   amet',
+                limited: '  Lorem    ipsum'
+            },
+            {
+                title: 'simple HTML',
+                input: '<p>Lorem ipsum dolor sit amet</p>',
+                unlimited: '<p>Lorem ipsum dolor sit amet</p>',
+                limited: '<p>Lorem ipsum</p>'
+            },
+            {
+                title: 'glued HTML',
+                input: '<p> Lorem </p><p> ipsum <br></p><p> dolor </p><p> sit </p><p> amet </p>',
+                unlimited: '<p> Lorem </p><p> ipsum <br></p><p> dolor </p><p> sit </p><p> amet </p>',
+                limited: '<p> Lorem </p><p> ipsum <br></p>'
+            }
+        ])
+        .test('limitByWordCount ', (data, assert) => {
+            assert.equal(strLimiter.limitByWordCount(data.input, 5), data.unlimited, 'Limited by 5 words');
+            assert.equal(strLimiter.limitByWordCount(data.input, 10), data.unlimited, 'Limited by 10 words');
+            assert.equal(strLimiter.limitByWordCount(data.input, 2), data.limited, 'Limited by 2 words');
+            assert.equal(strLimiter.limitByWordCount(data.input, 0), '', 'Limited by 0 words');
+            assert.equal(strLimiter.limitByWordCount(data.input, -2), '', 'Limited by negative value');
+        });
 
-    QUnit.cases.init([{
-        title: 'input already at the limit',
-        limit: 26,
-        input: 'Lorem ipsum dolor sit amet',
-        expected: 'Lorem ipsum dolor sit amet'
-    }, {
-        title: 'input below the limit',
-        limit: 100,
-        input: 'Lorem ipsum dolor sit amet',
-        expected: 'Lorem ipsum dolor sit amet'
-    }, {
-        title: 'input above the limit',
-        limit: 11,
-        input: 'Lorem ipsum dolor sit amet',
-        expected: 'Lorem ipsum'
-    // }, {
-    //     title: 'input already at the limit - with long spaces',
-    //     limit: 34,
-    //     input: 'Lorem    ipsum   dolor  sit   amet',
-    //     expected: 'Lorem    ipsum   dolor  sit   amet'
-    // }, {
-    //     title: 'input below the limit - with long spaces',
-    //     limit: 100,
-    //     input: 'Lorem    ipsum   dolor  sit   amet',
-    //     expected: 'Lorem    ipsum   dolor  sit   amet'
-    // }, {
-    //     title: 'input above the limit - with long spaces',
-    //     limit: 14,
-    //     input: 'Lorem    ipsum   dolor  sit   amet',
-    //     expected: 'Lorem    ipsum'
-    // }, {
-    //     title: 'input already at the limit - with tags',
-    //     limit: 26,
-    //     input: '<p>Lorem</p><p>ipsum</p><p>dolor</p><p>sit</p><p>amet</p>',
-    //     expected: '<p>Lorem</p><p>ipsum</p><p>dolor</p><p>sit</p><p>amet</p>'
-    // }, {
-    //     title: 'input below the limit - with tags',
-    //     limit: 100,
-    //     input: '<p>Lorem</p><p>ipsum</p><p>dolor</p><p>sit</p><p>amet</p>',
-    //     expected: '<p>Lorem</p><p>ipsum</p><p>dolor</p><p>sit</p><p>amet</p>'
-    // }, {
-    //     title: 'input above the limit - with tags',
-    //     limit: 11,
-    //     input: '<p>Lorem</p><p>ipsum</p><p>dolor</p><p>sit</p><p>amet</p>',
-    //     expected: '<p>Lorem</p><p>ipsum</p>'
-    // }, {
-    //     title: 'input already at the limit - with entities',
-    //     limit: 26,
-    //     input: '<p>Lorem&nbsp;ipsum&nbsp;dolor&nbsp;sit&nbsp;amet</p>',
-    //     expected: '<p>Lorem&nbsp;ipsum&nbsp;dolor&nbsp;sit&nbsp;amet</p>'
-    // }, {
-    //     title: 'input below the limit - with entities',
-    //     limit: 100,
-    //     input: '<p>Lorem&nbsp;ipsum&nbsp;dolor&nbsp;sit&nbsp;amet</p>',
-    //     expected: '<p>Lorem&nbsp;ipsum&nbsp;dolor&nbsp;sit&nbsp;amet</p>'
-    // }, {
-    //     title: 'input above the limit - with entities',
-    //     limit: 11,
-    //     input: '<p>Lorem&nbsp;ipsum&nbsp;dolor&nbsp;sit&nbsp;amet</p>',
-    //     expected: '<p>Lorem&nbsp;ipsum</p>'
-    }]).test('limitByCharCount ', (data, assert) => {
-        assert.equal(strLimiter.limitByCharCount(data.input, data.limit), data.expected, `Limit by ${data.limit} characters`);
-    });
+    QUnit.cases
+        .init([
+            {
+                title: 'plain text',
+                length: 26,
+                limit: 11,
+                input: 'Lorem ipsum dolor sit amet',
+                unlimited: 'Lorem ipsum dolor sit amet',
+                limited: 'Lorem ipsum'
+            },
+            {
+                title: 'plain text with long spaces',
+                length: 38,
+                limit: 19,
+                input: '  Lorem    ipsum   dolor  sit   amet  ',
+                unlimited: '  Lorem    ipsum   dolor  sit   amet  ',
+                limited: '  Lorem    ipsum   '
+            },
+            {
+                title: 'plain text with entities',
+                length: 36,
+                limit: 14,
+                input: 'Lorem &nbsp;&nbsp; ipsum &nbsp; dolor &eacute; sit &agrave; amet',
+                unlimited: 'Lorem &nbsp;&nbsp; ipsum &nbsp; dolor &eacute; sit &agrave; amet',
+                limited: 'Lorem &nbsp;&nbsp; ipsum'
+            },
+            {
+                title: 'simple HTML',
+                length: 26,
+                limit: 11,
+                input: '<p>Lorem ipsum dolor sit amet</p>',
+                unlimited: '<p>Lorem ipsum dolor sit amet</p>',
+                limited: '<p>Lorem ipsum</p>'
+            },
+            {
+                title: 'glued HTML',
+                length: 32,
+                limit: 14,
+                input: '<p> Lorem </p><p> ipsum <br></p><p> dolor </p><p> sit </p><p> amet </p>',
+                unlimited: '<p> Lorem </p><p> ipsum <br></p><p> dolor </p><p> sit </p><p> amet </p>',
+                limited: '<p> Lorem </p><p> ipsum <br></p>'
+            },
+            {
+                title: 'simple HTML with entities',
+                length: 36,
+                limit: 14,
+                input: '<p>Lorem &nbsp;&nbsp; ipsum &nbsp; dolor &nbsp; sit &nbsp; amet</p>',
+                unlimited: '<p>Lorem &nbsp;&nbsp; ipsum &nbsp; dolor &nbsp; sit &nbsp; amet</p>',
+                limited: '<p>Lorem &nbsp;&nbsp; ipsum</p>'
+            }
+        ])
+        .test('limitByCharCount ', (data, assert) => {
+            assert.equal(
+                strLimiter.limitByCharCount(data.input, data.length),
+                data.unlimited,
+                `Limit by ${data.length} characters`
+            );
+            assert.equal(strLimiter.limitByCharCount(data.input, 100), data.unlimited, `Limit by 100 characters`);
+            assert.equal(
+                strLimiter.limitByCharCount(data.input, data.limit),
+                data.limited,
+                `Limit by ${data.limit} characters`
+            );
+            assert.equal(strLimiter.limitByCharCount(data.input, 0), '', 'Limit by 0 characters');
+            assert.equal(strLimiter.limitByCharCount(data.input, -2), '', 'Limit by  negative value');
+        });
 });
