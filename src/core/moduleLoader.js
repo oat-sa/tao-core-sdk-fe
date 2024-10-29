ca0d676611b91bf244fecb21fabcb8bf99534d04
Kirill-Hatalski <kirill.gatalsky@taotesting.com>
2024-10-29 18:16:07 +0300

    chore:[WIP] dynamic import try, unit tests will DOWN

--- src/core/moduleLoader.js
+++ src/core/moduleLoader.js
@@ -177,14 +177,7 @@ export default function moduleLoaderFactory(requiredModules, validate, specs) {
                         });
                     } else {
                         return Promise.all(
-                            amdModules.map(module => {
-                                switch (module) {
-                                    case 'core/logger/console':
-                                        return import('./logger/console');
-                                    default:
-                                        return import(`${module}`);
-                                }
-                            })
+                            amdModules.map(module => import(`./${module}.js`))
                         ).then(loadedModules => loadedModules.map(module => module.default));
                     }
                 }
