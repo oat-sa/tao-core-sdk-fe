2732941c4732f792cc570dc63a7f5e2eabbf6e47
Yair Kukielka <yair.kukielka@taotesting.com>
2025-04-24 09:08:26 +0200

    fix: added more special characters to test

--- test/core/jwt/jwtToken/test.js
+++ test/core/jwt/jwtToken/test.js
@@ -42,15 +42,14 @@ define(['core/jwt/jwtToken'], jwtToken => {
         assert.equal(result.aud, 'www.example.com', 'aud correctly parsed');
     });
 
-    QUnit.test('parses payload object from full token with non-ASCII character ę', assert => {
+    QUnit.test('parses payload object from full token with non-ASCII characters ❤️ ñ å ę 🔥', assert => {
         assert.expect(4);
         const token =
-            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2MjA2NTM1NDgsImV4cCI6MTYyMDY1NDc2MiwiYXVkIjoid3d3LsSZeGFtcGxlLmNvbSIsInN1YiI6IiJ9.x4y4I7-lD-DsyQ4CAc0xTqSSTArQiHvntIDxwe6nkCA';
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpw7FlIEpXVCBCdWlsZGVyIOKdpO-4j_CflKUiLCJpYXQiOjE2MjA2NTM1NDgsImV4cCI6MTYyMDY1NDc2MiwiYXVkIjoid3d3LsSZeMOlbXBsZS5jb20iLCJzdWIiOiIifQ.g9h9kvy39vauIwM4S2i8jSuG0uRIVq0XpH9glMPoxN8';
         const result = parseJwtPayload(token);
         assert.ok(typeof result === 'object', 'parsed payload is an object');
-        assert.equal(result.iat, 1620653548, 'iat correctly parsed');
-        assert.equal(result.exp, 1620654762, 'exp correctly parsed');
-        assert.equal(result.aud, 'www.ęxample.com', 'aud correctly parsed');
+        assert.equal(result.iss, 'Onliñe JWT Builder ❤️🔥', 'iat correctly parsed');
+        assert.equal(result.aud, 'www.ęxåmple.com', 'aud correctly parsed');
     });
 
     QUnit.test('returns null for bad or missing token', assert => {
